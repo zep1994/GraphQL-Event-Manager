ef32dbe712d1638f82afb94568434de68caaa547
zep1994 <snaxbags@protonmail.com>
2019-03-11 13:06:22 -0500

    Setup local graphql

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use(
+    '/graphql', 
+    graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery{
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['All night coding', 'Gaming', 'Code again']
+        },
+        createEvent: (args) => {
+            const eventName = args.name
+            return eventName
+        }
+    },
+    graphiql: true
+}))
 
 app.get('/', (req, res, next) => {
     res.send('Hello World')
