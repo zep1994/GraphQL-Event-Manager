ddd273dd33bd7ce2db4db75519ffcb8b3a3c4f43
zep1994 <snaxbags@protonmail.com>
2019-03-13 11:25:34 -0500

    Setup MongoDB

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
+
+const Event = require('./models/event')
 
 const app = express()
-
-const events = []
 
 app.use(bodyParser.json())
 
@@ -43,18 +44,31 @@ app.use(
     `),
         rootValue: {
             events: () => {
-                return events
+                return Event.find()
+                    .then(events => {
+                        return events.map(event => {
+                            return { ...event._doc }
+                        })
+                    }).catch(err => {
+                        console.log(err)
+                    })
             },
-            createEvent: (args) => {
-                const event = {
-                    _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: args.eventInput.date
-                }  
-                events.push(event)  
-                return event        
+            createEvent: args => {
+                const event = new Event({
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: args.eventInput.date
+                })
+                return event
+                    .save()
+                    .then(result => {
+                        console.log(result)
+                        return { ...result._doc }
+                    }).catch(err => {
+                        console.log(err)
+                        throw err
+                    })
             }
         },
         graphiql: true
@@ -64,4 +78,16 @@ app.get('/', (req, res, next) => {
     res.send('Hello World')
 })
 
-app.listen(3000)
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${
+        process.env.MONGO_PASSWORD
+        }@cluster0-dlaom.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+    )
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
